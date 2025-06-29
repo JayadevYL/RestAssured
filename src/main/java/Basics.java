270b9872b4d4efa3976166fd4615b6a677ad7cd7
Jayadev YL <jayadevylgowda10@gmail.com>
2025-06-29 12:08:53 +0530

    Initial project commit

---
+++ src/main/java/Basics.java
@@ -0,0 +1,42 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class Basics {
+    public static void main(String[] args) {
+
+        RestAssured.baseURI="https://rahulshettyacademy.com";
+           String addCart= given().log().all().queryParam("key","qaclick123").header("content-type","application/json")
+                   .body("{\n" +
+                           "  \"location\": {\n" +
+                           "    \"lat\": -38.383494,\n" +
+                           "    \"lng\": 33.427362\n" +
+                           "  },\n" +
+                           "  \"accuracy\": 50,\n" +
+                           "  \"name\": \"Frontline house\",\n" +
+                           "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                           "  \"address\": \"29, side layout, cohen 09\",\n" +
+                           "  \"types\": [\n" +
+                           "    \"shoe park\",\n" +
+                           "    \"shop\"\n" +
+                           "  ],\n" +
+                           "  \"website\": \"http://google.com\",\n" +
+                           "  \"language\": \"French-IN\"\n" +
+                           "}").when().post("/maps/api/place/add/json")
+                    .then().log().all().assertThat().statusCode(200).body("status",equalTo("OK")).extract().response().asString();
+
+        JsonPath js=new JsonPath(addCart);
+        String placeId=js.getString("place_id");
+          String updateCart= given().queryParam("place_id",placeId).queryParam("key","qaclick123").header("content-type","application/json")
+                  .body("{\n" +
+                          "\"place_id\":\""+placeId+"\",\n" +
+                          "\"address\":\"70 winter walk, USA\",\n" +
+                          "\"key\":\"qaclick123\"\n" +
+                          "}")
+                   .when().put("maps/api/place/update/json")
+                   .then().assertThat().statusCode(200).extract().toString();
+    }
+}
---
+++ src/main/java/org/example/Practice/ResponseValidation.java
@@ -0,0 +1,43 @@
+package org.example.Practice;
+
+import io.restassured.path.json.JsonPath;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+1. Print No of courses returned by API
+
+2.Print Purchase Amount
+
+3. Print Title of the first course
+
+4. Print All course titles and their respective Prices
+
+5. Print no of copies sold by RPA Course
+
+6. Verify if Sum of all Course prices matches with Purchase Amount
+**/
+public class ResponseValidation {
+    public static void main(String[] args) {
+        JsonPath js=new JsonPath(Json.response());
+        int numberOfCourse=js.getInt("courses.size()");
+        System.out.println("numberOfCourse="+numberOfCourse);
+        int purchaseAmount=js.getInt("dashboard.purchaseAmount");
+        System.out.println("purchaseAmount="+purchaseAmount);
+        String titleOfFirstCourse=js.getString("courses[0].title");
+        System.out.println("First name of the Course="+titleOfFirstCourse);
+        System.out.println(List.of(js.getList("courses")));
+        // Extract list of courses as List of Map
+        List<Map<String, Object>> courses = js.getList("courses");
+
+        // Use Stream to find "RPA" course and print copies
+        int rpaCopies = courses.stream()
+                .filter(course -> course.get("title").equals("RPA"))
+                .map(course -> (Integer) course.get("copies"))
+                .findFirst()
+                .orElse(0);
+
+        System.out.println("Copies sold by RPA Course: " + rpaCopies);
+    }
+}
